515d933c02457e7fa896470bb384899711f399d9
Nikhil Sarvaiyya <nikhil.10704854@ltimindtree.com>
2024-11-15 10:11:46 +0530

    run app

--- app/angular-app/src/app/app.component.ts
+++ app/angular-app/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { VRAButton } from 'viralib';
+
 @Component({
   selector: 'app-root',
   standalone: true,
